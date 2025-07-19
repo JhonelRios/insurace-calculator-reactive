b019ff83225ca00f83da9df8177e999eccb81599
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2025-07-19 14:40:07 -0500

    feat: integration test

---
+++ src/test/java/com/jhonelrios/insurance_quote/IntegrationTest.java
@@ -0,0 +1,36 @@
+package com.jhonelrios.insurance_quote;
+
+import com.jhonelrios.insurance_quote.model.VehicleData;
+import com.jhonelrios.insurance_quote.util.JwtUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+public class IntegrationTest {
+    @Autowired
+    WebTestClient webClient;
+
+    @Autowired
+    JwtUtil jwtUtil;
+
+    @Test
+    void happyPathShouldWork() {
+        String token = jwtUtil.generateToken("test-user");
+
+        VehicleData data = new VehicleData("Toyota", "Yaris", 2020, "personal", 40);
+
+        webClient.post()
+                .uri("/api/quotes")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .bodyValue(data)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.brand").isEqualTo("Toyota");
+    }
+}
